5955cbb989b1e823f880d190201526568cd14890
seifmagdy2000 <seifmagdy20000@gmail.com>
2024-12-04 17:42:59 +0200

    routes defined

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const userRoutes = require("./userRoutes");
+const authRouter = require("./authRoutes");
 
 const router = express.Router();
 
 const baseUrl = "api/v1";
 
 router.use(`/${baseUrl}/users`, userRoutes.router);
-router.use(`/${baseUrl}/auth`, userRoutes.router);
+router.use(`/${baseUrl}/auth`, authRouter.router);
 
 module.exports = { router };
